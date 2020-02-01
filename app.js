92f1584ebb477535648c634465f94903221886de
sajan1231 <ashutoshsajan1213@gmail.com>
2020-02-01 18:31:38 +0530

    add proxy

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ if (process.env.NODE_ENV === 'development') {
   app.use(require('webpack-hot-middleware')(compiler));
 }
 
-app.use(cors());
+// app.use(cors());
 
 app.use('/api/v1', indexRouter);
 
