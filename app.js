b674133908bec5a1536919085ec02df56e5d4a26
sajan1231 <ashutoshsajan1213@gmail.com>
2020-02-01 18:09:23 +0530

    change start script to node from nodemon

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const indexRouter = require('./server/routes');
 
 const app = express();
 
-const connectDB = async () => {
+async function connectDB(){
 	try {
 		await mongoose.connect(process.env.MONGO_URI, {
 			useNewUrlParser: true,
