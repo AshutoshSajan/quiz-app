1e38c259c7fd463eb7ff23d0d5541d777a282935
AshutoshSajan <44765540+AshutoshSajan@users.noreply.github.com>
2021-11-09 11:40:50 +0530

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,17 @@
-const HtmlWebPackPlugin = require('html-webpack-plugin');
-const path = require('path');
-var webpack = require('webpack');
-const nodeExternals = require('webpack-node-externals');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require("path");
+var webpack = require("webpack");
+const nodeExternals = require("webpack-node-externals");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const htmlPlugin = new HtmlWebPackPlugin({
-  template: './client/public/index.html',
-  filename: './index.html'
+  template: "./client/public/index.html",
+  filename: "./index.html",
 });
 
 module.exports = {
-  mode: process.env.production ? 'production' : 'development',
-  entry: ['./client/src/index.js'],
+  mode: process.env.NODE_ENV,
+  entry: ["./client/src/index.js"],
   plugins: [htmlPlugin],
   module: {
     rules: [
@@ -19,45 +19,45 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
-        }
+          loader: "babel-loader",
+        },
       },
       {
         test: /\.s?css$/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
-        loader: 'file-loader',
+        loader: "file-loader",
         options: {
-          name: '/static/[name].[ext]'
-        }
-      }
-    ]
+          name: "/static/[name].[ext]",
+        },
+      },
+    ],
   },
   output: {
-    filename: 'bundle.js',
-    path: __dirname + '/dist/bundle/',
-    publicPath: '/static/'
+    filename: "bundle.js",
+    path: __dirname + "/dist/bundle/",
+    publicPath: "/static/",
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
     new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify(process.env.NODE_ENV)
-      }
+      "process.env": {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+      },
     }),
     new MiniCssExtractPlugin({
-      filename: 'bundle.css'
-    })
+      filename: "bundle.css",
+    }),
   ],
   resolve: {
-    extensions: ['.webpack.js', '.web.js', '.tsx', '.ts', '.js', '.json']
+    extensions: [".webpack.js", ".web.js", ".tsx", ".ts", ".js", ".json"],
   },
   externals: [nodeExternals()],
   externals: {
-    'react/lib/ReactContext': 'window',
-    'react/lib/ExecutionEnvironment': true,
-    'react/addons': true
-  }
+    "react/lib/ReactContext": "window",
+    "react/lib/ExecutionEnvironment": true,
+    "react/addons": true,
+  },
 };
