1778381b61a5d4923f50f6abf2613a59e3ab20cc
AshutoshSajan <44765540+AshutoshSajan@users.noreply.github.com>
2021-12-18 19:42:07 +0530

    move connect db to db-config

--- server/config/db-config.js
+++ server/config/db-config.js
@@ -19,21 +19,28 @@ const migrateDB = () => {
         // upsert: true,
       },
       (err, user) => {
-        console.log({ err, user });
+        // console.log({ err, user });
       },
     );
   });
 };
 
-const connectDb = (DB_URI) => {
-  const { connection } = mongoose;
+const connectDB = async (DB_URI) => {
+  try {
+    await mongoose.connect(DB_URI, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+    });
 
-  mongoose.connect(DB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
-
-  migrateDB();
+    migrateDB();
+    console.log('MongoDB Connected...');
+  } catch (err) {
+    console.error(err.message);
+    // Exit process with failure
+    process.exit(1);
+  }
 };
 
-module.exports = { connectDb };
+module.exports = { migrateDB, connectDB };
