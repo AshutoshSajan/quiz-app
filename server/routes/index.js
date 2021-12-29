20bf1b32e6a32c1128e813bab6f80aef88e3e392
AshutoshSajan <44765540+AshutoshSajan@users.noreply.github.com>
2021-12-29 21:44:53 +0530

    add post end point

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const usersRouter = require('./users');
 const quizRouter = require('./quizzes');
+const postRouter = require('./posts');
 
 router.use('/users', usersRouter);
 router.use('/quizzes', quizRouter);
+router.use('/posts', postRouter);
 
 module.exports = router;
