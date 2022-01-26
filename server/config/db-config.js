14427b56b1c3f75b6497129b0b29b39c94697185
AshutoshSajan <44765540+AshutoshSajan@users.noreply.github.com>
2022-01-26 19:45:25 +0530

    add mongoDB testion helper

--- server/config/db-config.js
+++ server/config/db-config.js
@@ -1,6 +1,21 @@
 // DB migration script to make changes in all the collections
 const mongoose = require('mongoose');
 
+const { NODE_ENV, MONGO_URI_DEV, MONGO_URI_PROD, MONGO_URI_TEST } = process.env;
+const mongoURI =
+  NODE_ENV === 'development'
+    ? MONGO_URI_DEV
+    : NODE_ENV === 'production'
+    ? MONGO_URI_PROD
+    : MONGO_URI_TEST;
+
+const options = {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+};
+
 const migrateDB = () => {
   const { collections } = mongoose.connections[0];
 
@@ -25,23 +40,36 @@ const migrateDB = () => {
   });
 };
 
-const connectDB = async (DB_URI) => {
+const connectDB = async () => {
   try {
-    await mongoose.connect(DB_URI, {
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(mongoURI, options);
 
     // migrateDB();
-    console.log('\nMongoDB Connected...\n');
+    console.log('mongodb Connected...');
   } catch (err) {
-    console.log('mongodb connection error', err.message);
-    console.error(err);
+    console.error('mongodb connection error', err);
     // Exit process with failure
     process.exit(1);
   }
 };
 
-module.exports = { migrateDB, connectDB };
+const mongoDB = {
+  mongoose,
+  connect: async () => {
+    try {
+      mongoose.Promise = Promise;
+      await mongoose.connect(mongoURI, options);
+
+      console.log('mongodb connected...');
+    } catch (err) {
+      console.error('mongodb connection error', err);
+      // Exit process with failure
+      process.exit(1);
+    }
+  },
+  disconnect: (done) => {
+    mongoose.disconnect(done);
+  },
+};
+
+module.exports = { migrateDB, connectDB, mongoDB };
