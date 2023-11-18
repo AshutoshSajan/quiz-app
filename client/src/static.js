4006b705140cb6fcea084389541a119f8d44cfc9
AshutoshSajan <44765540+AshutoshSajan@users.noreply.github.com>
2023-11-18 15:56:53 +0530

    update base url

--- client/src/static.js
+++ client/src/static.js
@@ -1,4 +1,4 @@
 export const BASE_URL =
   process.env.NODE_ENV === 'production'
-    ? 'https://quiz-app-node-react.herokuapp.com/api/v1'
+    ? 'https://node-react-quiz-app.onrender.com/api/v1'
     : 'http://localhost:3000/api/v1';
